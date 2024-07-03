05f0ec093f4be8f6b46de17748cf4d259ca26908
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-07-03 18:23:20 +0300

    wrote pep spider;  - add pep links scraping to parse;  - wrote parse pep from links below;

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -7,4 +9,17 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        all_peps_links = response.css(
+            '#numerical-index a.pep.reference.internal'
+        )
+        for pep_link in all_peps_links:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        title = response.css('h1.page-title::text').get().split(' – ')
+        data = {
+            'number': title[0][4:],
+            'name': title[1],
+            'status': response.css('dt:contains("Status")+dd abbr::text').get()
+        }
+        yield PepParseItem(data)
