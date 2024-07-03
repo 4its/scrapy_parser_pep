3a8cc493c1823bf2f9c909124993cee08f2bc3e7
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-07-03 21:42:32 +0300

    wrote pipline for pep;

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,32 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import datetime
+from collections import defaultdict
+from pathlib import Path
 
+# from itemadapter import ItemAdapter
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+BASE_DIR = Path(__file__).parent.parent
+FILE_PATH = BASE_DIR / 'results'
+TIME = datetime.datetime.now().strftime('%Y-%m-%dT%H-%M')
+FILE_NAME = f'status_summary_{TIME}.csv'
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        status_sums = defaultdict(int)
+        FILE_PATH.mkdir(exist_ok=True)
+
     def process_item(self, item, spider):
+        self.status_sums[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        file_output = FILE_PATH / FILE_NAME
+        with open(file_output, 'w', encoding='utf-8') as file:
+            csv.writer(file, dialect=csv.unix_dialect).writerows(
+                [
+                    ('Статус', 'Количество'),
+                    *self.status_sums.items(),
+                    ('Всего', sum(self.status_sums.values())),
+                ]
+            )
